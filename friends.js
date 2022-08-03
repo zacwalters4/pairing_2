1f616d6253b514eeb1baf16172dff61f7c6b9e2a
Zac Walters <zacwalters4@gmail.com>
2022-08-03 16:06:31 -0600

    Add places to visit

--- friends.js
+++ friends.js
@@ -3,3 +3,9 @@ var arrays = ["ozzy", "christian", "martin", "steve", "theo"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var visit = ["London", "Los Angeles", "Chicago", "Paris", "Hawaii"];
+
+visit.push("New York");
+
+console.log(visit);
